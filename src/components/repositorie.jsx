83820cb732ad4e73052c6ba3973b7b2303a210fd
Maycon Douglas <crossfireelite601@gmail.com>
2022-09-12 22:23:33 -0300

    inclusion of icons

--- src/components/repositorie.jsx
+++ src/components/repositorie.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect, useContext } from "react";
 import { GithubContext } from "../context/context";
 import { getRepo } from "../service/github";
 
+import { VscRepoForked } from "react-icons/vsc";
+import { FaRegStar } from "react-icons/fa";
 import { GiCircle } from "react-icons/gi";
 
 import "./repositorie.css";
@@ -9,6 +11,16 @@ import "./repositorie.css";
 const Repositorie = () => {
   const [dataRepositories, setDataRepositories] = useState([]);
   const [githubState] = useContext(GithubContext);
+
+  let date = new Date().getDate("2022-09-11T02:12:07Z");
+  console.log(date * 1000);
+
+  let ruby = "#701516";
+  let js = "#F1E05A";
+  let py = "#3572A5";
+  let c = "#555555";
+  let emcas = "#c065db";
+  let css = "#563d7c";
 
   useEffect(() => {
     const getData = async () => {
@@ -31,11 +43,25 @@ const Repositorie = () => {
           <div className="description">
             <span>{item.description}</span>
           </div>
-          <div className="language">
-            <span>
-              <GiCircle />
-              JavaScript
-            </span>
+          <div className="features">
+            <div className="language">
+              <span>
+                <GiCircle
+                  style={{ color: "#F1E05A", backgroundColor: "#F1E05A" }}
+                />
+                {item.language}
+              </span>
+            </div>
+            <div className="stars">
+              <span>
+                <FaRegStar e /> {item.stargazers_count}
+              </span>
+            </div>
+            <div className="forks">
+              <span>
+                <VscRepoForked /> {item.forks_count}
+              </span>
+            </div>
           </div>
         </div>
       ))}
